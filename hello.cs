46a7385d4cddd940649e5c12524ac7ecb354edc5
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2022-05-09 22:34:24 -0400

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello {
+  class hello {
+    static void main(string[] args) {
+      Console.WriteLine("Hello, world!);
+      Console.ReadLine();
+    }
+  }
+}
