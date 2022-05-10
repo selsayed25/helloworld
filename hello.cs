bf200e3c07fb4508792d8153efd3989f704b3691
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2022-05-10 08:49:50 -0400

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -1,16 +1,7 @@
 // This was coded by Sami Elsayed
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace hello {
-  class hello {
-    static void main(string[] args) {
-      Console.WriteLine("Hello, world!);
-      Console.ReadLine();
-    }
+class hello {
+  static void main() {
+    System.Console.WriteLine("Hello, world!");
   }
 }
